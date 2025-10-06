8b578ab5b42f6c39186e67c24babd0f4dfd72435
Suraj Sunar <surbhu06@gmail.com>
2025-10-06 18:36:10 +0600

    app: develop button reuse component and its stories

--- reuse-component/src/App.jsx
+++ reuse-component/src/App.jsx
@@ -1,35 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from "./components/ui/Button";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-100 h-screen space-y-8">
+      <div className="flex flex-col gap-y-4 justify-center items-center w-full">
+        <h1 className="text-4xl font-bold">Large Buttons</h1>
+        <div className="flex gap-6">
+          <Button theme="primary" size="lg">
+            Primary
+          </Button>
+          <Button theme="secondary" size="lg">
+            secondary
+          </Button>
+          <Button theme="danger" size="lg">
+            danger
+          </Button>
+          <Button theme="warning" size="lg">
+            warning
+          </Button>
+          <Button theme="success" size="lg">
+            success
+          </Button>
+          <Button theme="error" size="lg">
+            error
+          </Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col gap-y-4 justify-center items-center w-full">
+        <h1 className="text-4xl font-bold">Medium Buttons</h1>
+        <div className="flex gap-6">
+          <Button theme="primary" size="md">
+            primary
+          </Button>
+          <Button theme="secondary" size="md">
+            secondary
+          </Button>
+          <Button theme="danger" size="md">
+            danger
+          </Button>
+          <Button theme="warning" size="md">
+            warning
+          </Button>
+          <Button theme="success" size="md">
+            success
+          </Button>
+          <Button theme="error" size="md">
+            error
+          </Button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="flex flex-col gap-y-4 justify-center items-center w-full">
+        <h1 className="text-4xl font-bold">Small Buttons</h1>
+        <div className="flex gap-6">
+          <Button theme="primary" size="sm">
+            primary
+          </Button>
+          <Button theme="secondary" size="sm">
+            secondary
+          </Button>
+          <Button theme="danger" size="sm">
+            danger
+          </Button>
+          <Button theme="warning" size="sm">
+            warning
+          </Button>
+          <Button theme="success" size="sm">
+            success
+          </Button>
+          <Button theme="error" size="sm">
+            error
+          </Button>
+        </div>
+      </div>
 
-export default App
+      <div className="flex flex-col gap-y-4 justify-center items-center w-full">
+        <h1 className="text-4xl font-bold">Loading Buttons</h1>
+        <div className="flex gap-6">
+          <Button theme="primary" size="md" loading>
+            primary
+          </Button>
+          <Button theme="secondary" size="md" loading>
+            secondary
+          </Button>
+          <Button theme="danger" size="md" loading>
+            danger
+          </Button>
+          <Button theme="warning" size="md" loading>
+            warning
+          </Button>
+          <Button theme="success" size="md" loading>
+            success
+          </Button>
+          <Button theme="error" size="md" loading>
+            error
+          </Button>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-y-4 justify-center items-center w-full ">
+        <h1 className="text-4xl font-bold">Usage of Button</h1>
+        <div className="flex gap-6 flex-col w-1/2 rounded border border-gray-200 p-4">
+          <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis delectus officia natus soluta, velit obcaecati nostrum amet non suscipit. Fugit deserunt quisquam expedita beatae voluptatibus porro consequuntur iusto aut sint?
+          </div>
+          <Button theme="secondary" size="md" loading classes={'w-fit'}>
+            Read More ...
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ reuse-component/src/components/ui/Button.jsx
@@ -0,0 +1,30 @@
+import { LoaderCircle, Spline } from 'lucide-react'
+import React from 'react'
+
+const THEMES = {
+    primary: 'bg-gradient-to-r from-blue-500 via-blue-400 to-blue-500',
+    secondary: 'bg-gradient-to-r from-purple-500 via-purple-400 to-purple-500',
+    danger: 'bg-gradient-to-r from-red-500 via-red-400 to-red-500',
+    warning: 'bg-gradient-to-r from-yellow-500 via-yellow-400 to-yellow-500',
+    success: 'bg-gradient-to-r from-green-500 via-green-400 to-green-500',
+    error: 'bg-gradient-to-r from-red-800 via-red-400 to-red-500'
+}
+
+const SIZES = {
+    sm: 'px-2 py-1',
+    md: 'px-3 py-2',
+    lg: 'px-4 py-3',
+    xl: 'px-5 py-4'
+}
+
+const Button = ({children, classes, theme = 'primary', size = 'md', loading, onclick = ()=> null}) => {
+
+  return (
+    <button className={`${THEMES[theme]} ${SIZES[size]}  rounded-lg text-white capitalize flex gap-x-2 ${classes}`} onclick={onclick}>
+        {loading && <LoaderCircle className="animate-spin" />}
+        {children}
+    </button>
+  )
+}
+
+export default Button
