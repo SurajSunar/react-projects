51e3e45ab19096afe3887ebc7001f4bd3383ab26
Suraj Sunar <surbhu06@gmail.com>
2025-10-14 21:50:43 +0600

    app: component build

--- http-client-app/src/App.jsx
+++ http-client-app/src/App.jsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Card, Form, Input, Select } from "antd";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [form] = Form.useForm();
+  const [result, setResult] = useState()
+  const [error, setError] = useState()
+
+  const submitForm = async() => {
+    console.log(form.getFieldValue())
+
+    try {
+        const response = await axios.request({
+           method: form.method,
+           url: form.url
+        })
+        setResult(JSON.stringify(response.data, null, 2))
+
+    } catch (error) {
+      setError(error.status || 500)
+      toast.error('Network Issue', error)
+    }
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-100 min-h-screen">
+      <div className="w-1/2 mx-auto p-4">
+        <Card className="p-2">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold">Http API Testing</h1>
+            <p className="text-gray-500">
+              Access response from valid any Rest API
+            </p>
+            <Form onFinish={submitForm} form={form}>
+              <Form.Item
+                name="method"
+                rules={[
+                  { required: true, message: "Please input your method!" },
+                ]}
+              >
+                <Select>
+                  <Select.Option key={"GET"}>GET</Select.Option>
+                  <Select.Option key={"POST"}>POST</Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                name="url"
+                rules={[{ required: true, message: "Please input your URL!" }]}
+              >
+                <Input></Input>
+              </Form.Item>
 
-export default App
+              <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Card>
+      </div>
+      <ToastContainer/>
+    </div>
+  );
+};
+
+export default App;
