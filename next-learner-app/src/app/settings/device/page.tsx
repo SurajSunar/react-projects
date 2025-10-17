a01ea183794b4ba35014d57b588a094db864ce5a
Suraj Sunar <surbhu06@gmail.com>
2025-10-17 15:46:59 +0600

    app: added device page

---
+++ next-learner-app/src/app/settings/device/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DevicePage = () => {
+  return (
+    <div>DevicePage - This page is understanded nested routing in Next</div>
+  )
+}
+
+export default DevicePage
