59a81536c0bca1c03b845b261e02ed2a81783e72
Suraj Sunar <surbhu06@gmail.com>
2025-10-21 15:28:55 +0600

    app: added cart mutation

--- next-learner-app/src/app/carts/listing/page.tsx
+++ next-learner-app/src/app/carts/listing/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 const CartListPage = () => {
   const { isPending, error, data } = useQuery({
@@ -10,12 +11,48 @@ const CartListPage = () => {
       fetch("https://dummyjson.com/carts").then((res) => res.json()),
   });
 
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: () => {
+      return fetch("https://dummyjson.com/carts/add", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: 1,
+          products: [
+            {
+              id: 144,
+              quantity: 4,
+            },
+            {
+              id: 98,
+              quantity: 1,
+            },
+          ],
+        }),
+      });
+    },
+    onSuccess: () => {
+      toast.success("Added to cart");
+    },
+    onError: () => {
+      toast.error("Error in adding to cart");
+    },
+  });
+
   if (isPending) {
     return <div>Loading carts...</div>;
   }
 
   return (
     <div className="flex flex-col gap-6 m-2 ">
+      <button
+        className="border border-gray-200 py-2 px-4 w-fit"
+        onClick={() => {
+          mutation.mutate();
+        }}
+      >
+        Add Product
+      </button>
       {data.carts.map((cart: any) => (
         <div className="flex flex-col gap-4 bg-cyan-200  p-4 rounded-xl">
           <h1>totalProducts: {cart.totalProducts}</h1>
@@ -30,6 +67,7 @@ const CartListPage = () => {
           </div>
         </div>
       ))}
+      <ToastContainer />
     </div>
   );
 };
