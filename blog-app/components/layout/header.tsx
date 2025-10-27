8c1e3b7410d6bd4e8d780537fb5b4e4dbd707e27
Suraj Sunar <surbhu06@gmail.com>
2025-10-27 16:35:42 +0600

    app: adding condition to render menu

--- blog-app/components/layout/header.tsx
+++ blog-app/components/layout/header.tsx
@@ -3,8 +3,6 @@
 import { signOut, useSession } from "@/lib/auth-client";
 import Link from "next/link";
 import UserMenu from "../auth/user-menu";
-import { useTheme } from "next-themes";
-import { Lightbulb, LightbulbOff } from "lucide-react";
 import ThemeToggle from "../theme/theme-toggle";
 
 const navItems = [
@@ -28,16 +26,21 @@ export default function Header() {
           <Link href={"/"} className="text-2xl font-bold">
             Blog App
           </Link>
-          <nav className="hidden md:flex items-center gap-6">
-            {navItems.map((nav) => (
-              <Link key={nav.href} href={nav.href} className="hover:font-bold">
-                {nav.label}
-              </Link>
-            ))}
-          </nav>
+          {data && (
+            <nav className="hidden md:flex items-center gap-6">
+              {navItems.map((nav) => (
+                <Link
+                  key={nav.href}
+                  href={nav.href}
+                  className="hover:font-bold"
+                >
+                  {nav.label}
+                </Link>
+              ))}
+            </nav>
+          )}
         </div>
         <div className="flex items-center gap-4">
-          <div>Search here...</div>
           <div>
             <ThemeToggle />
           </div>
