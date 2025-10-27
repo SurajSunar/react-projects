d5a7d3588cae5198356014c25db9a697b7cebe68
Suraj Sunar <surbhu06@gmail.com>
2025-10-27 16:34:40 +0600

    app: embedding profile component

--- blog-app/app/profile/page.tsx
+++ blog-app/app/profile/page.tsx
@@ -1,5 +1,11 @@
+import Profiledetails from "@/components/profile/profile-details";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="m-6">
+      <Profiledetails />
+    </div>
+  );
 };
 
 export default ProfilePage;
