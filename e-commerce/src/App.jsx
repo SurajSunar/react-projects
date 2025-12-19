7d12d6bc8180d0427b84862e3d89b18ddf3e6ea5
Suraj Sunar <surbhu06@gmail.com>
2025-12-19 14:50:11 +0600

    app: added routes

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import "animate.css";
+import "@ant-design/v5-patch-for-react-19";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AdminLogin from "./components/admin/AdminLogin";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/admin">
+            <Route path="login" element={<AdminLogin />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
