50e4ddafa09dab8ccc921a73f7394780395ef605
Suraj Sunar <surbhu06@gmail.com>
2025-12-23 21:07:53 +0600

    app: styling 404 page

--- e-commerce/src/404.jsx
+++ e-commerce/src/404.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="bg-gray-200 flex justify-center items-center text-gray-500 h-screen animate__animated animate__fadeIn animate__slower">
+      404 | Page Not Found
+    </div>
+  );
 };
 
 export default NotFound;
