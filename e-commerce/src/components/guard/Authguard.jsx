8396018849853350fafca625fd688e0139e9d929
Suraj Sunar <surbhu06@gmail.com>
2025-12-24 19:05:41 +0600

    app: added auth logic changes

--- e-commerce/src/components/guard/Authguard.jsx
+++ e-commerce/src/components/guard/Authguard.jsx
@@ -2,15 +2,25 @@ import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../zustand/useAuth";
 import { Loader2 } from "lucide-react";
+import axios from "axios";
 
 const Authguard = () => {
   const { user } = useAuth();
   const [isLogin, setIsLogin] = useState(null);
+  const [role, setRole] = useState();
+  const isAdmin = role === "admin";
 
   useEffect(() => {
     const validateUser = async () => {
       try {
-        setIsLogin(!!user?.token);
+        const { data } = await axios.get("http://localhost:3000/verify-token", {
+          headers: {
+            Authorization: "Bearer " + user.accessToken,
+          },
+        });
+
+        setIsLogin(data.valid);
+        setRole(data?.user?.role);
       } catch (error) {
         console.log(error);
         setIsLogin(false);
@@ -27,11 +37,25 @@ const Authguard = () => {
       </div>
     );
   } else if (isLogin === true) {
-    return window.location.pathname === "/login" ? (
-      <Navigate to={"/admin/dashboard"} />
-    ) : (
-      <Outlet />
-    );
+    if (isAdmin) {
+      if (
+        window.location.pathname === "/login" ||
+        window.location.pathname === "/"
+      ) {
+        return <Navigate to={"/admin/dashboard"} />;
+      } else {
+        return <Outlet />;
+      }
+    } else {
+      if (
+        window.location.pathname === "/login" ||
+        window.location.pathname.includes("/admin")
+      ) {
+        return <Navigate to={"/"} />;
+      } else {
+        return <Outlet />;
+      }
+    }
   } else {
     return window.location.pathname === "/login" ? (
       <Outlet />
