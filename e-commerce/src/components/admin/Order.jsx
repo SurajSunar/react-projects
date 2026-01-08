bacda29bb3543e446598eeebd4324bc01fdd4b02
Suraj Sunar <surbhu06@gmail.com>
2026-01-08 21:29:58 +0600

    orders: rendering order list

--- e-commerce/src/components/admin/Order.jsx
+++ e-commerce/src/components/admin/Order.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 
 const Order = () => {
-  const [orders, setOrders] = useState();
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     const loadOrders = async () => {
@@ -12,7 +12,16 @@ const Order = () => {
     loadOrders();
   }, []);
 
-  return <div>{orders}</div>;
+  return (
+    <div>
+      {orders &&
+        orders.map((order, index) => (
+          <>
+            {index + 1}. {order.products} - Nu.{order.cost}
+          </>
+        ))}
+    </div>
+  );
 };
 
 export default Order;
